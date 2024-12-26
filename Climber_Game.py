93fa7279ddf5b7952fc9785bb174981c9bb6b8cf
Curtis Mesmer <curtis@Curtis-MacBook.local>
2024-12-26 08:54:07 -0800

    doing stuff

---
+++ Climber_Game.py
@@ -0,0 +1,22 @@
+## Climbing Game -- Fun Project
+## One player type of game, kind of exploring projects..
+## If a move fails, the climber dies
+## There is a climber, a project with moves
+## takes climbers levels (endurace, pwoer, sterngth, and PE) and is tested on the climbers requirments
+
+
+climbers_name = input("What is your name: ")
+print(climbers_name)
+
+class Climber:
+    pass
+
+class Rock_Climb:
+    pass
+
+
+## Make sure to:
+# git init
+# git add
+# git commit -m " "
+
