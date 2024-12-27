a91e394846ee976dd46256557e205296abee0018
Curtis Mesmer <curtis@Curtis-MacBook.local>
2024-12-27 08:33:00 -0800

    updating

--- Climber_Game.py
+++ Climber_Game.py
@@ -17,10 +17,11 @@
 # --------------------------------------------------------------------------- #
 
 ##  Classes
- 
+
+
 class Climber:
 
-    def __init__(self, name, style, str, pow, end, powend):
+    def __init__(self, name, style, str, pow, end, powend, health = 100): 
         self.name = name
         self.style = style
         self.str = str
@@ -31,7 +32,17 @@ class Climber:
     def __repr__(self):
         return f"New climber {self.name}, {self.style}, stats: Str{self.str}; Pow{self.pow}; End{self.end}; PowEnd{self.powend}" ## Better update needed
 
-    ## Will add more stuff
+    def climb_attempt(self):
+        pass
+
+    ## Ideas
+
+    def lose_health(self):
+        pass
+
+    def died(self):
+        pass
+    
 
 class Rock_Climb:
 
@@ -45,40 +56,48 @@ class Rock_Climb:
     ## Maybe climbs can be built were certain stats are required to make moves.. 
 
     ## Will add more stuff
+
+    ## Working with ideas
+        # Trying to create climbs requirements based on stats
+        # if climber attempts without meet requirements, they die.. 
+
+    def attempt_climb(self, climber):
+
+        ## Requires 8 strength to complete..
+        ## WIll only do this one to see how easy it works (or not)..
+
+        requirement = self.difficulty * .25 ## A stat needs to be 25% of diff  (arbitrary)   
+    
+        while True:
+            if climber.str < requirement:
+                print("Not Strong enough")
+                break
+            elif climber.pow < requirement:
+                print("Now Powerful enough")
+                break
+            elif climber.end < requirement:
+                print("Now Enduro enough")
+                break
+            elif climber.powend < requirement:
+                print("Now Power Enduro enough")
+                break
+            else:
+                print("Send it!")
+                break
+        
+# =========>>>  DAVID
+## If you'd like, create a random class that interacts with stuff..
+
+
 # --------------------------------------------------------------------------- #
 
-## Testing
-      
-## Test Climber
-rico = Climber("Enrico", "balls-limic", 7, 7, 9, 8)
-#print(rico)
-#print(rico.str)
-
-## Test Rock_Climb
-v_zero = Rock_Climb("Testy")
-#print(v_zero)
-#print(v_zero.difficulty)
-
-## Test Climber on Climb -- if stats don't sum up to/ greater than difficulty .. climber falls and dies
-
-## Have to figure out how to automatically pull each stat. 
-def attempting_climb(climber, climb):
-    total_stats = climber.str + climber.pow + climber.end + climber.powend
-    # return total_stats
-    if total_stats >= climb.difficulty:
-        print(f"{climber} successfully climb {climb}, and didn't die")
-    else:
-        print(f"{climber} died climbing {climb}")
-
-
-attempting_climb(rico, v_zero) # testing function
-
-# --------------------------------------------------------------------------- #
 
 ## Game Intro
 
-# climber profiles
-# strenght, power, endurance, power endurance
+# Saving these for later -- > climber profiles
+
+## Dummy climber
+Rico = Climber("Enrico", "balls-limic", 7, 7, 9, 8)
 
 #Strength climber
 Jesse = Climber("Jesse", "OTC", 10, 8, 4, 7) #total 29
@@ -103,6 +122,22 @@ Magnus = ("Campus Everything", 7, 9, 5, 8)
 Gruper = ("No power, never tired", 5, 5, 10, 7)
 
 Megos = ("So German", 7, 7, 5, 10)
+
+
+# --------------------------------------------------------------------------- #
+
+# Climbs to use
+
+## Test Rock Climb
+v_zero = Rock_Climb("Testy")
+
+## A harder Rock Climb
+segatron = Rock_Climb("Segatron", 30)
+
+## Slab is for Kings
+dandie_smearon = Rock_Climb("Dandie Smearon", 40)  ## 40 is the highest number
+
+## NOTES: How can I set these up -- to contain specific requirements.. (maybe to difficult for me atm).. Lets just climb for now.. 
 
 # --------------------------------------------------------------------------- #
                
@@ -133,9 +168,47 @@ entry_climber
 
 stats = tot_stats[0] ## messed up .... maybe can stream line this..
 
-total_package = Climber(entry_climber, stats[0], stats[1], stats[2], stats[3], stats[4])
-print(total_package)
+
+
+player_one = Climber(entry_climber, stats[0], stats[1], stats[2], stats[3], stats[4])  ## Need to change the name of this
+print(player_one)
     
+# --------------------------------------------------------------------------- #
+
+## Testing
+      
+
+#print(rico)
+#print(rico.str)
+
+
+#print(v_zero)
+#print(v_zero.difficulty)
+
+## Test Climber on Climb -- if stats don't sum up to/ greater than difficulty .. climber falls and dies
+
+## Have to figure out how to automatically pull each stat. 
+def attempting_climb(climber, climb):
+    total_stats = climber.str + climber.pow + climber.end + climber.powend
+    # return total_stats
+    if total_stats >= climb.difficulty:
+        print(f"{climber} successfully climb {climb}, and didn't die")
+    else:
+        print(f"{climber} died climbing {climb}")
+
+
+attempting_climb(Rico, v_zero) # testing function
+
+# --------------------------------------------------------------------------- #
+
+## Playing the game
+
+#Starting
+play_the_game = input("Your stats are set.. Attemp a climb? (Yes or No)")
+
+#Picking a climb (v_zero; segatron; dandie_smearon)
+
+
 # --------------------------------------------------------------------------- #
 
 ## Git commands
